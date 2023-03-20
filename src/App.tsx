9ea9e5e539df5d533c630ad50a5d93dfdfa0b118
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-03-21 00:04:40 +0530

    [ add ] : sidebar design implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+
 function App() {
-  return <div className="text-center">hi</div>;
+  return (
+    <div className="h-screen w-screen overflow-hidden">
+      <div className="grid grid-cols-11 h-screen w-screen overflow-hidden">
+        <div className="col-span-3">
+          <Sidebar />
+        </div>
+        <div className="col-span-8"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
